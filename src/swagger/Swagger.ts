3430c525d4fc99efefa00d974e489294bd216455
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-01-04 16:00:50 -0500

    Force Swagger No port, https requests

--- src/swagger/Swagger.ts
+++ src/swagger/Swagger.ts
@@ -14,7 +14,7 @@ const Swagger: (fastify: FastifyInstance) => Promise<void> = async (fastify: Fas
                 version: JSON.parse(await fs.readFile(new URL("../../package.json", import.meta.url), "utf-8")).version
             },
             servers: [
-                { url: `http://${process.env.SWAGGER_HOST || 'localhost'}:${process.env.SWAGGER_PORT || '9443'}`, description: 'Localhost' }
+                { url: `https://${process.env.SWAGGER_HOST || 'localhost'}`, description: `${process.env.SWAGGER_HOST || 'localhost'}` }
             ]
         }
     });
